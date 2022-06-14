cf4fe7338afc9982aa3da565f7da8c3f0ffbffab
Zsolt Molnar <ysooolt@gmail.com>
2022-06-14 16:18:29 +0200

    Adding extra IAM configuration

--- step0/tf-master-account-iam/main.tf
+++ step0/tf-master-account-iam/main.tf
@@ -12,5 +12,48 @@ module "s3_bucket" {
   versioning = {
     enabled = true
   }
+}
 
+provider "aws" {
+  profile = "org-assumer"
+  region = "eu-central-1"
+  alias = "wl1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::234539872435:role/OrganizationAccountAccessRole"
+  }
+}
+
+provider "aws" {
+  profile = "org-assumer"
+  region = "eu-central-1"
+  alias = "wl2"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::957694108706:role/OrganizationAccountAccessRole"
+  }
+}
+
+module "iam_user_wl1" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  providers = {
+    aws = aws.wl1
+  }
+
+  name          = "workload-admin"
+}
+
+module "iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  providers = {
+    aws = aws.wl2
+  }
+
+  name          = "workload-admin"
+  force_destroy = true
+}
+
+output "pgp_key" {
+  value = module.iam_user
+  sensitive = true
 }
